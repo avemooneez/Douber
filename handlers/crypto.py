53092e701d08beb9827ef1e922c15eb264fa5271
Dmitry <mooneez.mnz@gmail.com>
2024-04-10 01:52:19 +0300

    создание middleware для антифлуда + создание хранилища redis

--- handlers/crypto.py
+++ handlers/crypto.py
@@ -2,9 +2,13 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram import Router, F
 from utils import crypto
+from middlewares.antiflood import AntiFloodMiddleware
+from aiogram.fsm.storage.redis import RedisStorage
 
+storage = RedisStorage.from_url("redis://localhost:6379/0")
 router = Router()
 router.message.filter(F.chat.type.in_({"private"}))
+router.message.middleware(middleware=AntiFloodMiddleware(storage=storage))
 
 @router.message(Command("crypto"))
 async def cmd_crypto(message: Message):
---
+++ middlewares/antiflood.py
@@ -0,0 +1,27 @@
+from typing import Any, Awaitable, Callable, Dict
+from aiogram.types import Message, TelegramObject
+from aiogram import BaseMiddleware
+from aiogram.fsm.storage.redis import RedisStorage
+
+
+class AntiFloodMiddleware(BaseMiddleware):
+
+    def __init__(self, storage: RedisStorage):
+        self.storage = storage
+    
+    async def __call__(self,
+                       handler: Callable[[TelegramObject,Dict[str, Any]], Awaitable[Any]],
+                       event: Message,
+                       data: Dict[str, Any]
+                       ) -> Any:
+        user = f"user{event.from_user.id}"
+
+        check_user = await self.storage.redis.get(name=user)
+        if check_user:
+            if int(check_user.decode()) == 1:
+                await self.storage.redis.set(name=user, value=1, ex=5)
+                return await event.answer("Пиши медленнее!")
+            return
+        await self.storage.redis.set(name=user, value=1, ex=5)
+
+        return await handler(event, data)
