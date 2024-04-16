2684035b9ff3b468c06aae71fc37630f2068654b
Dmitry <mooneez.mnz@gmail.com>
2024-04-16 17:16:16 +0300

    изменение форматирования в donate

--- handlers/donate.py
+++ handlers/donate.py
@@ -1,12 +1,15 @@
 from aiogram.types import Message
 from aiogram import Router, F
+from aiogram.enums import ParseMode
 
 router = Router()
 
 @router.message(F.text == "Донат")
 async def cbq_crypto(message: Message):
-    await message.reply("BTC: ```1AdDq1DfvzxRzWwfMdss1392pSqZKfLbY1```\n"
-                        "USDT TRC20: ```TQCivxEGG32st2P7dDce4cqNTkcFxksexw```\n"
-                        "DonationAlerts: donationalerts.com/r/avemooneez\n"
-                        "Сбор Тинькофф: tinkoff.ru/cf/fou6EWkgYB\n",
-                        disable_web_page_preview=True)
+    await message.reply(
+        """BTC: `1AdDq1DfvzxRzWwfMdss1392pSqZKfLbY1`
+USDT TRC20: `TQCivxEGG32st2P7dDce4cqNTkcFxksexw`
+DonationAlerts: donationalerts.com/r/avemooneez
+Сбор Тинькофф: tinkoff.ru/cf/fou6EWkgYB""",
+                        disable_web_page_preview=True,
+                        parse_mode=ParseMode.MARKDOWN)
