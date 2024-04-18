99b736f4718722079c315bd39ecd15cb757b798f
Dmitry <mooneez.mnz@gmail.com>
2024-04-18 14:34:06 +0300

    более удобное форматирование

--- handlers/profile.py
+++ handlers/profile.py
@@ -8,11 +8,18 @@ from db import Database
 router = Router()
 db = Database("./database.db")
 
+def negative_limit(tokens):
+    if tokens > 50000:
+        return 0
+    else:
+        return '{0:,}'.format(50000 - tokens)
+
 @router.message(Command("profile"))
 @router.message(F.text == "Профиль")
 async def cmd_profile(message: Message):
     model = db.get_model(message.from_user.id)
     tokens = db.get_used_tokens(message.from_user.id)
+
     msg_template = """Профиль @{username}
 
 <i><b>GPT:</b></i>
@@ -23,7 +30,7 @@ async def cmd_profile(message: Message):
 Чтобы получить больше токенов необходимо поддержать проект, а скриншот о донате отправить @mnz_pr."""
     msg = msg_template.format(username=message.from_user.username,
                               model=model[0][0],
-                              limit=tokens[0][0],
+                              limit=negative_limit(tokens[0][0]),
                               month=get_next_month()
                               )
     await message.reply(f"{msg}", parse_mode=ParseMode.HTML)
