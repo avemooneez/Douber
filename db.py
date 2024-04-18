a561961fc8947f280869da6e844e8c51f1c72ba7
Dmitry <mooneez.mnz@gmail.com>
2024-04-18 14:52:53 +0300

    рефакторинг кода

--- db.py
+++ db.py
@@ -54,19 +54,29 @@ tokens INTEGER DEFAULT (0)
     
     def user_exists(self, user_id: int):
         with self.conn:
-            result = self.cur.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
+            result = self.cur.execute(
+                "SELECT * FROM `users` WHERE `user_id` = ?",
+                (user_id,)).fetchmany(1)
             return bool(len(result))
     
     def add_user(self, user_id: int):
         with self.conn:
-            self.cur.execute("INSERT INTO `users` (user_id) VALUES (?)", (user_id,))
-            self.cur.execute("INSERT INTO `settings` (user_id) VALUES (?)", (user_id,))
-            self.cur.execute("INSERT INTO `gpt` (user_id, version) VALUES (?, ?)", (user_id, "gpt-3.5-turbo-0613",))
+            self.cur.execute(
+                "INSERT INTO `users` (user_id) VALUES (?)",
+                (user_id,))
+            self.cur.execute(
+                "INSERT INTO `settings` (user_id) VALUES (?)",
+                (user_id,))
+            self.cur.execute(
+                "INSERT INTO `gpt` (user_id, version) VALUES (?, ?)",
+                (user_id, "gpt-3.5-turbo-0613",))
             return
     
     def get_selectedCryptos(self, user_id: int):
         with self.conn:
-            return self.cur.execute("SELECT `BTCUSDT`, `ETHUSDT`, `TONUSDT`, `SOLUSDT`, `ADAUSDT`, `DOGEUSDT` FROM `settings` WHERE `user_id` = ?", (user_id,)).fetchall()
+            return self.cur.execute(
+                "SELECT `BTCUSDT`, `ETHUSDT`, `TONUSDT`, `SOLUSDT`, `ADAUSDT`, `DOGEUSDT` FROM `settings` WHERE `user_id` = ?",
+                (user_id,)).fetchall()
     
     def edit_selectedCryptos(self, user_id: int, selCrpt: str, OnOff: bool):
         with self.conn:
@@ -83,12 +93,12 @@ tokens INTEGER DEFAULT (0)
             if selCrpt == "TON" and OnOff == True:
                 return self.cur.execute("UPDATE `settings` SET `TONUSDT` = ? WHERE `user_id` = ?", ('1', user_id,))
             elif selCrpt == "TON" and OnOff == False:
-                return self.cur.execute("UPDATE `settingss` SET `TONUSDT` = ? WHERE `user_id` = ?", ('0', user_id,))
+                return self.cur.execute("UPDATE `settings` SET `TONUSDT` = ? WHERE `user_id` = ?", ('0', user_id,))
             
             if selCrpt == "SOL" and OnOff == True:
                 return self.cur.execute("UPDATE `settings` SET `SOLUSDT` = ? WHERE `user_id` = ?", ('1', user_id,))
             elif selCrpt == "SOL" and OnOff == False:
-                return self.cur.execute("UPDATE `settingss` SET `SOLUSDT` = ? WHERE `user_id` = ?", ('0', user_id,))
+                return self.cur.execute("UPDATE `settings` SET `SOLUSDT` = ? WHERE `user_id` = ?", ('0', user_id,))
             
             if selCrpt == "ADA" and OnOff == True:
                 return self.cur.execute("UPDATE `settings` SET `ADAUSDT` = ? WHERE `user_id` = ?", ('1', user_id,))
@@ -100,17 +110,27 @@ tokens INTEGER DEFAULT (0)
             elif selCrpt == "DOGE" and OnOff == False:
                 return self.cur.execute("UPDATE `settings` SET `DOGEUSDT` = ? WHERE `user_id` = ?", ('0', user_id,))
 
+
     def add_used_tokens(self, user_id: int, used_tokens: int):
         with self.conn:
-            old_used_tokens = self.cur.execute("SELECT `tokens` FROM `gpt` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
-            self.cur.execute("UPDATE `gpt` SET `tokens` = ? WHERE `user_id` = ?", ((old_used_tokens[0][0] + used_tokens), user_id,))
+            old_used_tokens = self.cur.execute(
+                "SELECT `tokens` FROM `gpt` WHERE `user_id` = ?",
+                (user_id,)).fetchmany(1)
+            
+            self.cur.execute(
+                "UPDATE `gpt` SET `tokens` = ? WHERE `user_id` = ?",
+                ((old_used_tokens[0][0] + used_tokens), user_id,))
             return
     
     def get_used_tokens(self, user_id: int):
         with self.conn:
-            return self.cur.execute("SELECT `tokens` FROM `gpt` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
+            return self.cur.execute(
+                "SELECT `tokens` FROM `gpt` WHERE `user_id` = ?",
+                (user_id,)).fetchmany(1)
     
     def get_model(self, user_id: int):
         with self.conn:
-            return self.cur.execute("SELECT `version` FROM `gpt` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
+            return self.cur.execute(
+                "SELECT `version` FROM `gpt` WHERE `user_id` = ?",
+                (user_id,)).fetchmany(1)
 
