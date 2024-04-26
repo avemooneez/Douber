907945279c346a696de216a6b15ce67a1aa94f93
Dmitry <mooneez.mzn@gmail.com>
2024-04-26 18:46:59 +0300

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def main():
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 
     db = Database("./database.db")
-    db.start() # start database
+    db.start()
     
     bot = Bot(token=tokens.bot_token)
     dp = Dispatcher()
