82cd218637f1881e34a0d22ad6a9134e6dff59b1
Dmitry <mooneez.mnz@gmail.com>
2024-05-21 00:30:23 +0300

    переход с использования команды на кнопку для использования функции

--- handlers/urlshort.py
+++ handlers/urlshort.py
@@ -1,20 +1,28 @@
 from aiogram.types import Message
 from aiogram.filters import Command, CommandObject
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.filters import StateFilter
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 from utils.urlshort import short_url
+
 
 router = Router()
 
-@router.message(Command("url"))
-async def cmd_urlshort(message: Message, command: CommandObject):
-    link = command.args
-    if link == None:
-        await message.reply(
-            "Неверное использование: /url <ссылка>"
-        )
-        return
-    url = short_url(url=link)
+class ShortURL(StatesGroup):
+    short = State()
+
+@router.message(StateFilter(None), F.text == "Сократить ссылку")
+async def cmd_url(message: Message, state: FSMContext):
+    await message.reply("Введите ссылку")
+    await state.set_state(ShortURL.short)
+
+@router.message(ShortURL.short)
+async def response(message: Message, state: FSMContext):
+    url = short_url(url=message.text)
     if url != "Please enter a valid URL.":
         await message.reply(url)
     else:
         await message.reply("К сожалению, ссылка не валидна. Попробуйте ещё раз")
+    await state.set_state(None)
+    return
